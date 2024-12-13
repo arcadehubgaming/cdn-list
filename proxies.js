f419b474599120ed98b03b9836f5938cf66e03b8
chyves <82107846+notchyves@users.noreply.github.com>
2024-12-13 14:17:54 -0500

    shadow browser new

--- proxies.js
+++ proxies.js
@@ -12,6 +12,22 @@ ArcadeHubItems.Proxies = [
         "url": "https://base.diveyez.com/"
     },
     {
+        "name": "Shadow Browser (Server #3)",
+        "url": "https://jny7gjz5kqft4ed0taizldewa1g3lfskm9gauwpfzyosch3ydezt6tftwyptdgw.cederfeldt.se/"
+    },
+    {
+        "name": "Shadow Browser (Server #4)",
+        "url": "https://math.focr.sg/"
+    },
+    {
+        "name": "Shadow Browser (Server #5)",
+        "url": "https://jny7gjz5kqft4ed0taizldewa1g3lfskm9gauwpfzyosch3ydezt6tftwyptdgw.cederfeldt.se/"
+    },
+    {
+        "name": "Shadow Browser (Server #6)",
+        "url": "https://education.jodymaroni.com/"
+    },
+    {
         "name": "Emerald (Server #1)",
         "url": "https://howcanibebetteratlifewhenidonthaveajob.pagostepeapulco.gob.mx/"
     },
