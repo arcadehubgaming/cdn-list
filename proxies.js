f185c2ed55bd4c6871d6e59f3f52e98ffd02f4c1
chyves <82107846+notchyves@users.noreply.github.com>
2024-12-13 14:14:47 -0500

    a few more

--- proxies.js
+++ proxies.js
@@ -8,11 +8,27 @@ ArcadeHubItems.Proxies = [
         "url": "https://evony.laviewddns.com/"
     },
     {
+        "name": "Shadow Browser (Server #2)",
+        "url": "https://base.diveyez.com/"
+    },
+    {
         "name": "Emerald (Server #1)",
         "url": "https://howcanibebetteratlifewhenidonthaveajob.pagostepeapulco.gob.mx/"
     },
     {
         "name": "Emerald (Server #2)",
         "url": "https://vs.ntpromo.ro/"
+    },
+    {
+        "name": "Emerald (Server #3)",
+        "url": "https://shiniesongreen.69.mu/"
+    },
+    {
+        "name": "Emerald (Server #3)",
+        "url": "https://shiniesongreen.69.mu/"
+    },
+    {
+        "name": "Interstellar (Server #1)",
+        "url": "https://housepools.power-media.ro/"
     }
 ];
