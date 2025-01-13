ba6ff6c956c07760c00ff0af1429d92170b52365
chyves <82107846+notchyves@users.noreply.github.com>
2025-01-13 08:55:12 -0500

    Update proxies.js

--- proxies.js
+++ proxies.js
@@ -44,5 +44,9 @@ ArcadeHubItems.Proxies = [
         "name": "Interstellar (Server #2)",
         "url": "https://dartn-zako.laviewddns.com/",
         "noaboutblank": true
+    },
+    {
+        "name": "Szvy Central v2",
+        "url": "https://zearn.global.ssl.fastly.net/"
     }
 ];
