c58df90d038e228f90b44b8b88dcde7c92ddda4f
chyves <82107846+notchyves@users.noreply.github.com>
2025-02-24 10:42:03 -0500

    Update proxies.js

--- proxies.js
+++ proxies.js
@@ -4,44 +4,48 @@ ArcadeHubItems.Proxies = [
         "url": "https://base.diveyez.com/"
     },
     {
-        "name": "Shadow Browser (Server #2)",
-        "url": "https://jny7gjz5kqft4ed0taizldewa1g3lfskm9gauwpfzyosch3ydezt6tftwyptdgw.cederfeldt.se/"
-    },
-    {
-        "name": "Shadow Browser (Server #3)",
-        "url": "https://math.focr.sg/"
-    },
-    {
-        "name": "Shadow Browser (Server #4)",
-        "url": "https://jny7gjz5kqft4ed0taizldewa1g3lfskm9gauwpfzyosch3ydezt6tftwyptdgw.cederfeldt.se/"
-    },
-    {
-        "name": "Shadow Browser (Server #5)",
-        "url": "https://education.jodymaroni.com/"
-    },
-    {
-        "name": "TinF0il (Server #1) (Most Recommended)",
-        "url": "https://finti0lajpisd.global.ssl.fastly.net/",
-        "noaboutblank": true
-    },
-    {
-        "name": "TinF0il (Server #2)",
+        "name": "TinF0il (Server #1)",
         "url": "http://schoology-math.ekharipati.com.np/",
         "noaboutblank": true
     },
     {
-        "name": "TinF0il (Server #3)",
-        "url": "https://view.lithiumis.us/",
+        "name": "Interstellar (Server #1)",
+        "url": "https://matheiuslikesmen.bouncers4rent.com/",
         "noaboutblank": true
     },
     {
-        "name": "TinF0il (Server #4)",
-        "url": "https://play.lexia-power-up.us/",
+        "name": "Szvy Central (Server #1)",
+        "url": "https://goofyahh.global.ssl.fastly.net/",
         "noaboutblank": true
     },
     {
-        "name": "TinF0il (Server #5)",
-        "url": "https://point-blank-should-have-won-proxathon.point-blank.us.kg/",
+        "name": "Szvy Central (Server #2)",
+        "url": "https://englishwork4school.mka.in/",
         "noaboutblank": true
-    }
+    },
+    {
+        "name": "Szvy Central (Server #3)",
+        "url": "https://tap.ne-t.org/",
+        "noaboutblank": true
+    },
+    {
+        "name": "Szvy Central (Server #4)",
+        "url": "https://jaseiszestyasflip.global.ssl.fastly.net/",
+        "noaboutblank": true
+    },
+    {
+        "name": "Szvy Central (Server #5)",
+        "url": "https://app.need-some.e-visionweb.com/",
+        "noaboutblank": true
+    },
+    {
+        "name": "Szvy Central (Server #6)",
+        "url": "https://dfsjoifjdsffjdfjsd.tennis.fennis.tk/",
+        "noaboutblank": true
+    },
+    {
+        "name": "Szvy Central (Server #7)",
+        "url": "https://zearn.math-for.7s.com.tr/",
+        "noaboutblank": true
+    },
 ];
